e5cd0caddaf23ed6a55d840c3c696e8f326104d3
Mikewillv94 <mikeumsm@gmail.com>
2020-12-16 16:04:27 -0300

    movimentacao-disco

--- script.js
+++ script.js
@@ -7,10 +7,6 @@
  let torre3 = document.createElement("div")
  torre3.classList.add("item");
 
- document.getElementById('section').appendChild(torre1);
- document.getElementById('section').appendChild(torre2);
- document.getElementById('section').appendChild(torre3);
-
  let a = document.createElement("div");
  a.classList.add("discoUm");
  torre1.appendChild(a);
@@ -27,14 +23,29 @@
  a4.classList.add("discoQuatro");
  torre1.appendChild(a4);
 
+ document.getElementById('section').appendChild(torre1);
+ document.getElementById('section').appendChild(torre2);
+ document.getElementById('section').appendChild(torre3);
 
  
 
 
-//let testeStart = document.getElementById(torre1);
 
-//testeStart.addEventListener('click',  Start);
+let move = null;
+let disco = null;
 
-//function Start(event){
-//    console.log(event.currentTarget.lastElementChild);
-//}
+torre1.addEventListener('click', Start);
+function Start(e){    
+    e.currentTarget;
+    move = e.currentTarget.lastElementChild;
+    console.log(move);
+}
+
+torre2.addEventListener('click', Start2);
+function Start2(e){    
+    e.currentTarget;
+    disco = e.currentTarget;
+    console.log(disco);
+    disco.appendChild(move);
+}
+
