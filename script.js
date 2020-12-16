7352097e623e5d945ef3b9caccffb6e661170a4a
Mikewillv94 <mikeumsm@gmail.com>
2020-12-16 00:46:23 -0300

    fornt-test

---
+++ script.js
@@ -0,0 +1,31 @@
+ let a = document.createElement("a");
+ a.style.backgroundColor = 'orange';
+ a.style.height = '30px';
+ a.style.width = '90%';
+ a.style.borderRadius = "2px";
+
+ document.getElementById("item1").appendChild(a);
+
+ let a2 = document.createElement("a");
+ a2.style.backgroundColor = 'blue';
+ a2.style.height = '30px';
+ a2.style.width = '70%';
+ a2.style.borderRadius = "2px";
+
+ document.getElementById("item1").appendChild(a2);
+
+ let a3 = document.createElement("a");
+ a3.style.backgroundColor = 'yellow';
+ a3.style.height = '30px';
+ a3.style.width = '50%';
+ a3.style.borderRadius = "2px";
+
+ document.getElementById("item1").appendChild(a3);
+
+ let a4 = document.createElement("a");
+ a4.style.backgroundColor = 'red';
+ a4.style.height = '30px';
+ a4.style.width = '30%';
+ a4.style.borderRadius = "2px";
+
+ document.getElementById("item1").appendChild(a4);
