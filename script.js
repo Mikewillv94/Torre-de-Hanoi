894ecfb23f836e9c30f0e6c4430e7307065a3bda
Mikewillv94 <mikeumsm@gmail.com>
2020-12-17 14:51:06 -0300

    ajuste de moviemnto dos discos

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let torre1 = document.createElement("div")
 torre1.classList.add("item");
+torre1.id = 't1';
 
 let torre2 = document.createElement("div")
 torre2.classList.add("item");
+torre2.id = 't2';
 
 let torre3 = document.createElement("div")
 torre3.classList.add("item");
+torre3.id = 't3';
 
 let a = document.createElement("div");
 a.classList.add("discoUm");
@@ -28,15 +31,37 @@ document.getElementById('section').appendChild(torre2);
 document.getElementById('section').appendChild(torre3);
 
 
-    torre1.addEventListener('click', getStart);
-    function getStart(e) {
-        disco = e.currentTarget.lastElementChild;
-        console.log(disco);
-    }
+torre1.addEventListener('click', getDisco);
+torre2.addEventListener('click', getDisco);
+torre3.addEventListener('click', getDisco);
 
-    torre2.addEventListener('click', setStart);
-    function setStart(e) {
-        e.currentTarget.appendChild(disco);
-        console.log(disco);
+let disco;
+
+function getDisco(e) {
+
+    disco = e.currentTarget.lastElementChild;
+    console.log(disco);
+    if(disco !== null){
+        torre1.removeEventListener('click', getDisco);
+        torre2.removeEventListener('click', getDisco);
+        torre3.removeEventListener('click', getDisco);
+    
+        torre1.addEventListener('click', setDisco);
+        torre2.addEventListener('click', setDisco);
+        torre3.addEventListener('click', setDisco);
     }
+}
+
+function setDisco(e) {
+    e.currentTarget.appendChild(disco);
+    torre1.removeEventListener('click', setDisco);
+    torre2.removeEventListener('click', setDisco);
+    torre3.removeEventListener('click', setDisco);
+
+    torre1.addEventListener('click', getDisco);
+    torre2.addEventListener('click', getDisco);
+    torre3.addEventListener('click', getDisco);
+
+}
+
 
